0a24ab56d9bfd74162b62bd5046b6a4b00f5c224
Pesadomxtras <148004713+Pesadomxtras@users.noreply.github.com>
2023-10-15 13:42:54 -0600

    Delete src directory

--- src/utils/jwt.ts
+++
@@ -1,9 +0,0 @@
-const STORAGE_KEY = `${process.env.REACT_APP_MY_APP_ID}:jwt`;
-
-export const setJWT = (jwt: string) => {
-  window.localStorage.setItem(STORAGE_KEY, jwt);
-};
-
-export const getJWT = (): string | null => {
-  return window.localStorage.getItem(STORAGE_KEY);
-};
